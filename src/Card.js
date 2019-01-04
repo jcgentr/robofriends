02f187b725df0ea5bc0f64fdfd4d5b8191eed23d
jcgentr <jcgentr@clemson.edu>
2019-01-04 16:37:44 -0500

    add Card component

---
+++ src/Card.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Card = ({ id, name, email }) => { // destructing props; ES6
+	
+	return ( // only JSX: html-like syntax using JS; JSexpressions use { }
+		// code must be wrapped in one div element to return; src uses JS templated string
+		<div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+			<img alt='robot' src={`https://robohash.org/${id}?size=200x200`} /> 
+			<div>
+				<h2>{name}</h2>
+				<p>{email}</p>
+			</div>
+		</div>
+	);
+}
+
+export default Card;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'; // the react machine under the hood
+import ReactDOM from 'react-dom'; // dom for website view manipulation; native for mobile
 import './index.css';
-import App from './App';
+// import App from './App'; // assumes .js file
+import Card from './Card';
 import * as serviceWorker from './serviceWorker';
+import 'tachyons';
+import { robots } from './robots'; // {robots} called destructuring (need if export >= 1 from js file)
 
-ReactDOM.render(<h1>Hello World! from the react app</h1>, document.getElementById('root'));
-// <App />
+ReactDOM.render(<div>
+					<Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
+					<Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
+					<Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
+					<Card id={robots[3].id} name={robots[3].name} email={robots[3].email}/>
+				</div>, 
+document.getElementById('root')); // Hello component with property greeting
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
